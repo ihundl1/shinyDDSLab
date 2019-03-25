c798c28e8bd20ce4f11f5f13696213a93c82da65
Isabella <isabella.n.hundley@gmail.com>
2019-03-25 15:02:39 -0500

    Temporary update for current semester

--- betterBackend.R
+++ betterBackend.R
@@ -3,13 +3,19 @@ library(tidyverse)
 source('connection.R')
 
 #import data
-nameTable <- tbl(conDatasource, 'vwRoster') %>% select(pawsId, fullname, sectionName) %>% collect()
+nameTable <- tbl(conDatasource, 'vwRoster')
 big <- tbl(conDatasource, 'vwBig') %>% collect()
 subs <- tbl(conDatasource, 'vwSubs') %>% collect() %>% mutate(eNum = substr(label, 2, 2))
 assignments <- tbl(conDatasource,'exchunk') %>% filter(mainTopic == 'excel') %>% collect()
 attendance <- tbl(conDatasource, 'vwStudentDate') %>% filter(!is.na(eventDate)) %>% collect()
 dates <- tbl(conDatasource, 'attevent') %>% select(eventId, eventDate) %>% collect() %>%
   mutate(section = paste0("Section ", substr(eventId, 7, 7)))
+
+# Fix for current semester
+nameTable <- nameTable %>% filter(substr(section, 1, 6) == "2019SP") %>% 
+  select(pawsId, fullname, sectionName) %>% collect()
+big <- big %>% filter(substr(section, 1, 6) == "2019SP")
+dates <- dates %>% filter(substr(eventId, 1, 6) == "2019SP")
 
 # Inputs
 ## Students by Section
