dc4f71e32a9129d6cfa3035b313e3d511944d357
Isabella <isabella.n.hundley@gmail.com>
2019-04-11 11:39:32 -0500

    oneInput updated to match twoInputs

--- oneInput.R
+++ oneInput.R
@@ -6,11 +6,12 @@ library(scales)
 source('betterBackend.R')
 
 studentSelect <- setNames(nameTable$pawsId, nameTable$fullname)
+exSelect <- setNames(exVector, exVector)
 
 ui <- dashboardPage(
   # App Title
-  dashboardHeader(title = "ISDS 1102 Instructor Dashboard"),
-
+  dashboardHeader(title = dashTitle, titleWidth = 450),
+  
   # Sidebar for Inputs
   dashboardSidebar(
     disable = TRUE
@@ -20,8 +21,9 @@ ui <- dashboardPage(
   dashboardBody(
     # create a row
     fluidRow(
-      box(selectInput("student", "Select a Student", c(" " = "", studentSelect)), width = 8),
-      valueBoxOutput("attValue", width = 4)
+      box(selectInput("student", "Select a Student", c(" " = "", studentSelect)), width = 6),
+      box(selectInput("ex", "Select an Exercise", c(" " = "", exSelect)), width = 3),
+      valueBoxOutput("attValue", width = 3)
     ),
     fluidRow(
       box(plotOutput("submissions", height = 200), width = 12)
@@ -55,18 +57,28 @@ server <- function(input, output) {
   
   # Update & Generate Submissions Tables
   studentSubs <- reactive(filter(subs, pawsId == input$student) %>% 
-                            select(label, submissions, bestScore, eNum))
+                            select(label, submissions, bestScore, eNum) %>% mutate(type = "student"))
   emptyAssignments <- reactive(filter(assignments, !(label %in% studentSubs()$label)) %>%
                                  mutate(submissions = 0, bestScore = as.integer(0)) %>%
-                                 mutate(eNum = substr(label, 2, 2)) %>%
-                                 select(label, submissions, bestScore, eNum))
-  fullSubs <- reactive(rbind(studentSubs(), emptyAssignments()) %>% 
-                         filter(eNum <= max(studentSubs()$eNum)))
+                                 mutate(type = "student") %>%
+                                 select(label, submissions, bestScore, eNum, type))
+  sectionAverages <- reactive(filter(classAvg, type == as.character(
+    filter(nameTable, pawsId == input$student) %>% select(section)
+  )))
+  maxEx <- reactive(ifelse(input$ex == "", max(studentSubs()$eNum), input$ex))
+  fullSubs <- reactive(rbind(studentSubs(), emptyAssignments(), sectionAverages()) %>% 
+                         filter(eNum <= maxEx()))
   
   # Generate attendance table for chart
   studentAttendance <- reactive(filter(attendance, pawsId == input$student))
   dateValues <- reactive(filter(dates, section == studentSection()) %>% 
                            mutate(attended = ifelse(eventDate %in% studentAttendance()$eventDate, 1, 0)))
+  
+  # Colors!
+  colorPalette <- reactive(ifelse(attWarning() == 'red', "Reds", 
+                                  ifelse(attWarning() == 'orange', "Oranges", "Blues")))
+  colorPicker <- reactive(ifelse(attWarning() == 'red', "red3", 
+                                 ifelse(attWarning() == 'orange', "darkorange2", "royalblue3")))
   
   # generate value box
   output$attValue <- renderValueBox({
@@ -76,16 +88,17 @@ server <- function(input, output) {
   
   # generate plots
   output$submissions <- renderPlot({
-    ggplot(studentSubs(), aes(x = label, y = bestScore)) +
-      geom_col(fill = "#AED6F1") + 
-      geom_text(data = filter(fullSubs(), submissions > 0),
-                aes(label = submissions), position = position_stack(vjust = .5), size = 10) + 
+    ggplot(fullSubs(), aes(x = label, y = bestScore, fill = type)) +
+      geom_col(position = "dodge") + 
+      geom_text(aes(y = bestScore/2, label = submissions), position = position_dodge(width = 0.9),
+                size = 6) + 
       theme_minimal() + ylab("Best Score") + xlab("Chunk") +
-      theme(panel.grid.major.x = element_blank())
+      theme(panel.grid.major.x = element_blank()) +
+      scale_fill_brewer(palette = colorPalette())
   })
   output$attChart <- renderPlot({
     ggplot(dateValues(), aes(x = eventDate, y = attended)) + 
-      geom_col(fill = attWarning()) + theme_minimal() + 
+      geom_col(fill = colorPicker()) + theme_minimal() + 
       ylab("Attended") + xlab("Class Date") +
       theme(panel.grid.major.x = element_blank())
   })
